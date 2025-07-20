8bd419cae91d8332883eeb4ebf69582ebe477afb
loristhe9th <ttklinh611@gmail.com>
2025-07-20 18:51:48 +0700

    Updated Mascot.tsx

--- src/components/ui/Mascot.tsx
+++ src/components/ui/Mascot.tsx
@@ -40,7 +40,7 @@ export default function Mascot({
         stroke="#3B2D4C"
         strokeWidth="6"
         strokeLinejoin="round"
-        transform="rotate(37 95 80)"
+        transform="rotate(35 95 80)"
       />
 
       {/* Eyes */}
